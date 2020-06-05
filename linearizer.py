0baae78c725a949b5060e5740ba32e2c25e694b2
Jakob Lindqvist <jakob.lindqvist@chalmers.se>
2020-06-05 10:56:29 +0200

    Added Identity linearizer

--- linearizer.py
+++ linearizer.py
@@ -3,7 +3,7 @@
 The KF relies on some form of linearization of
 motion and measurement models, e.g.:
 
-    - Unit, when models are linear (affine) to begin with
+    - Identity, when models are linear (affine) to begin with
     - Analytical, such as the EKF
     - Sigma point methods
     - SLR
@@ -32,3 +32,16 @@ class Linearizer(ABC):
                 x' = Ax + b + eps, eps ~ N(0, Sigma)
         """
         pass
+
+
+class Identity(Linearizer):
+    """Identity linearizer
+    Trivial linearizer for linear (affine) motion and meas models
+    """
+    def __init__(self, A, b, Sigma):
+        self.A = A
+        self.b = b
+        self.Sigma = Sigma
+
+    def linear_params(self, _mean, _cov):
+        return (self.A, self.b, self.Sigma)
