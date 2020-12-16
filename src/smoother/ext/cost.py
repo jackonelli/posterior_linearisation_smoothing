33adffc0b46bda279fbc5ddeb2b0fbdaed8529c6
Jakob <jakob.lindqvist@chalmers.se>
2020-12-16 15:26:11 +0100

    Vectorised cost a bit more.

--- src/smoother/ext/cost.py
+++ src/smoother/ext/cost.py
@@ -2,30 +2,15 @@ import numpy as np
 
 
 def cost(means, measurements, m_1_0, P_1_0, motion_model, meas_model):
-    diff = means[0, :] - m_1_0
-    _cost = diff.T @ np.linalg.inv(P_1_0) @ diff
+    prior_diff = means[0, :] - m_1_0
+    _cost = prior_diff.T @ np.linalg.inv(P_1_0) @ prior_diff
+
+    proc_diff = means[1:, :] - motion_model.map_set(means[:-1, :])
+    meas_diff = measurements - meas_model.map_set(means)
     for k in range(0, means.shape[0] - 1):
-        diff = means[k + 1, :] - motion_model.mapping(means[k, :])
-        _cost += diff.T @ np.linalg.inv(motion_model.proc_noise(k)) @ diff
+        _cost += proc_diff[k, :].T @ np.linalg.inv(motion_model.proc_noise(k)) @ proc_diff[k, :]
         # measurements are zero indexed, i.e. k-1 --> y_k
-        diff = measurements[k, :] - meas_model.mapping(means[k, :])
-        _cost += diff.T @ np.linalg.inv(meas_model.meas_noise(k)) @ diff
-    diff = measurements[-1, :] - meas_model.mapping(means[-1, :])
-    _cost += diff.T @ np.linalg.inv(meas_model.meas_noise(k)) @ diff
-
-    return _cost
-
-
-def ml_cost(means, measurements, m_1_0, P_1_0, motion_model, meas_model, Q, R):
-    diff = means[0, :] - m_1_0
-    _cost = diff.T @ np.linalg.inv(P_1_0) @ diff
-    for k in range(0, means.shape[0] - 1):
-        diff = means[k + 1, :] - motion_model(means[k, :])
-        _cost += diff.T @ np.linalg.inv(Q) @ diff
-        # measurements are zero indexed, i.e. k-1 --> y_k
-        diff = measurements[k, :] - meas_model(means[k, :])
-        _cost += diff.T @ np.linalg.inv(R) @ diff
-    diff = measurements[-1, :] - meas_model(means[-1, :])
-    _cost += diff.T @ np.linalg.inv(R) @ diff
+        _cost += meas_diff[k, :].T @ np.linalg.inv(meas_model.meas_noise(k)) @ meas_diff[k, :]
+    _cost += meas_diff[-1, :].T @ np.linalg.inv(meas_model.meas_noise(k)) @ meas_diff[-1, :]
 
     return _cost
