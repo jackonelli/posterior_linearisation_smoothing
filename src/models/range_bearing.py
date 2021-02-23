f83847ee1966214dcecfda95c41b2ba6c32aa5c2
Jakob <jakob.lindqvist@chalmers.se>
2021-02-23 13:42:08 +0100

    Common Jacobians for range/bear, arb sensor number

--- src/models/range_bearing.py
+++ src/models/range_bearing.py
@@ -13,7 +13,7 @@ class RangeBearing(MeasModel, Differentiable):
 
     def mapping(self, state, time_step=None):
         range_ = np.sqrt(np.sum((state[:2] - self.pos) ** 2))
-        bearing = angle(state, self.pos)
+        bearing = _angle(state, self.pos)
         return np.array([range_, bearing])
 
     def meas_noise(self, time_step):
@@ -24,11 +24,8 @@ class RangeBearing(MeasModel, Differentiable):
         s_x, s_y = self.pos[0], self.pos[1]
         delta_x, delta_y = x - s_x, y - s_y
 
-        range_den = np.sqrt((delta_x) ** 2 + (delta_y) ** 2)
-        range_diff = np.array([delta_x / range_den, delta_y / range_den])
-
-        bearings_diff = atan2_jacobian(delta_x, delta_y)
-
+        range_diff = _euclid_dist_jacobian(delta_x, delta_y)
+        bearings_diff = _angle_jacobian(delta_x, delta_y)
         non_zero = np.row_stack((range_diff, bearings_diff))
 
         return np.column_stack((non_zero, np.zeros((2, 3))))
@@ -39,29 +36,23 @@ class MultiSensorRange(MeasModel, Differentiable):
         """
         Num. sensors = N
         sensors (np.ndarray): (N, D_y)
-
-        TODO: Actually extend to arb. num sensors (curr two (Jacobian))
         """
         self.sensors = sensors
         self._meas_noise = meas_noise
 
     def mapping(self, state, time_step=None):
-        return np.apply_along_axis(lambda pos: euclid_dist(state[:2], pos), axis=1, arr=self.sensors)
+        return np.apply_along_axis(lambda pos: _euclid_dist(state[:2], pos), axis=1, arr=self.sensors)
 
     def meas_noise(self, time_step):
         return self._meas_noise
 
     def jacobian(self, state, time_step=None):
         zeros_len = state.shape[0] - 2
-        s_1 = self.sensors[0, :]
-        s_2 = self.sensors[1, :]
-        s_1_den = np.sqrt((state[0] - s_1[0]) ** 2 + (state[1] - s_1[1]) ** 2)
-        s_2_den = np.sqrt((state[0] - s_2[0]) ** 2 + (state[1] - s_2[1]) ** 2)
-        H_11 = -(s_1[0] - state[0]) / s_1_den
-        H_12 = -(s_1[1] - state[1]) / s_1_den
-        H_21 = -(s_2[0] - state[0]) / s_2_den
-        H_22 = -(s_2[1] - state[1]) / s_2_den
-        return np.column_stack((np.array([[H_11, H_12], [H_21, H_22]]), np.zeros((2, zeros_len))))
+        x, y = state[0], state[1]
+        non_zero = np.apply_along_axis(
+            lambda pos: _euclid_dist_jacobian(x - pos[0], y - pos[1]), axis=1, arr=self.sensors
+        )
+        return np.column_stack((non_zero, np.zeros((2, zeros_len))))
 
     def sample(self, states):
         means = self.map_set(states)
@@ -75,14 +66,12 @@ class MultiSensorBearings(MeasModel, Differentiable):
         """
         Num. sensors = N
         sensors (np.ndarray): (N, D_y)
-
-        TODO: Actually extend to arb. num sensors (curr two (Jacobian))
         """
         self.sensors = sensors
         self._meas_noise = meas_noise
 
     def mapping(self, state, time_step=None):
-        return np.apply_along_axis(lambda pos: angle(state[:2], pos), axis=1, arr=self.sensors)
+        return np.apply_along_axis(lambda pos: _angle(state[:2], pos), axis=1, arr=self.sensors)
 
     def meas_noise(self, time_step):
         return self._meas_noise
@@ -90,7 +79,7 @@ class MultiSensorBearings(MeasModel, Differentiable):
     def jacobian(self, state, time_step=None):
         zeros_len = state.shape[0] - 2
         x, y = state[0], state[1]
-        non_zero = np.apply_along_axis(lambda pos: atan2_jacobian(x - pos[0], y - pos[1]), axis=1, arr=self.sensors)
+        non_zero = np.apply_along_axis(lambda pos: _angle_jacobian(x - pos[0], y - pos[1]), axis=1, arr=self.sensors)
 
         return np.column_stack((non_zero, np.zeros((self.sensors.shape[0], zeros_len))))
 
@@ -101,19 +90,24 @@ class MultiSensorBearings(MeasModel, Differentiable):
         return means + noise
 
 
-def atan2_jacobian(x, y):
-    num = x ** 2 + y ** 2
-    return np.array([-y / num, x / num])
-
-
-def euclid_dist(p_1, p_2):
+def _euclid_dist(p_1, p_2):
     return np.sqrt(np.sum((p_1 - p_2) ** 2))
 
 
-def angle(p_1, p_2):
+def _euclid_dist_jacobian(x, y):
+    den = np.sqrt(x ** 2 + y ** 2)
+    return np.array([x / den, y / den])
+
+
+def _angle(p_1, p_2):
     delta_x = p_1[0] - p_2[0]
     delta_y = p_1[1] - p_2[1]
     return np.arctan2(delta_y, delta_x)
+
+
+def _angle_jacobian(x, y):
+    den = x ** 2 + y ** 2
+    return np.array([-y / den, x / den])
 
 
 def to_cartesian_coords(meas, pos):
