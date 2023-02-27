8c009c2ea777253baa321764088c087cec56af12
Jakob <jakob.lindqvist@chalmers.se>
2023-02-27 10:53:39 +0100

    Updated README and reduced log-level in exp.

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ It also provides implementations for related papers
 
 ## Setup
 
-The python dependencies are managed by [`pipenv`](https://pipenv.pypa.io/en/latest/)
+The python dependencies are managed by [`pipenv`](https://pipenv.pypa.io/en/latest/),
+see `Pipfile` for the requirements.
 
 ```
 # Get the source code
@@ -15,10 +16,19 @@ cd post_lin_smooth
 pipenv shell
 ```
 
-### Data
+## Recreate experiments:
 
 Results published in the the paper can be reproduced by running the corresponding script in the `exp` sub-dir.
 Using a fixed seed will guarantee exactly replicated data and conditions.
+
+- Figure 2. Simulated coordinated turn with bearings only measurements
+  ```bash
+  python exp/coord_turn/realisation.py --meas_type bearings --num_iter 10
+  ```
+- Figure 4. Visualisation of a single realisation of the CT experiment with varying bearings only measurements.
+  ```bash
+  python exp/coord_turn/realisation.py --meas_type bearings --var_sensors --num_iter 10
+  ```
 
 For previous results that this code reproduces a fixed seed is not possible (the previous papers' experiments are exclusively implemented in matlab).
 For them the actual data used in those trials are included in this repo (the `data` sub-dir).
@@ -26,19 +36,14 @@ For them the actual data used in those trials are included in this repo (the `da
 ## Test coverage
 
 Explicit unit testing as such, is limited at the moment.
-There is, however, a few larger tests, akin to integration tests.
-These are automated verification that the library implementation faithfully reproduces the results published in the papers describing the implemented filter/smoother.
+There is, however, multiple larger tests, akin to integration tests.
+These are automated verification that the library implementation faithfully reproduces the results published in the papers describing the various implemented filter/smoother.
 All filters and smoothers (more or less) use the same base methods in the end.
-A test veryfying that a smoother in this library produces the same results as those in a peer reviewed paper, is a strong indication that the underlying base methods are correct.
-Especially when every value is accurate over a long sequence, in multiple dimensions for many iterations.
+A test veryfying that a smoother in this library produces the same results as those in peer reviewed papers, is a strong indication that the underlying base methods are correct.
+Especially when every value is accurate over a long sequence, in multiple dimensions and for many iterations.
 
-## Recreate experiments:
+To run all the tests, run (from the repo. root):
 
-- Figure 1. Simulated coordinated turn with bearings only measurements
-  ```bash
-  python exp/coord_turn/realisation.py --meas_type bearings --num_iter 10
-  ```
-- Figure 3. Visualisation of a single realisation of the CT experiment with varying bearings only measurements.
-  ```bash
-  python exp/coord_turn/realisation.py --meas_type bearings --var_sensors --num_iter 10
-  ```
+```bash
+python -m unittest
+```
--- exp/coord_turn/realisation.py
+++ exp/coord_turn/realisation.py
@@ -46,7 +46,7 @@ def main():
     args = parse_args()
     log = logging.getLogger(__name__)
     experiment_name = "ct_experiment_realisation"
-    setup_logger(f"logs/{experiment_name}.log", logging.DEBUG)
+    setup_logger(f"logs/{experiment_name}.log", logging.INFO)
     log.info(f"Running experiment: {experiment_name}")
     if not args.random:
         np.random.seed(2)
@@ -116,43 +116,16 @@ def main():
     D_x = prior_mean.shape[0]
     K = len(measurements)
     init_traj = (np.zeros((K, D_x)), np.array(K * [prior_cov]))
-    # log.info("Running IEKS...")
-    # ms_ieks, Ps_ieks, cost_ieks, rmses_ieks, neeses_ieks = run_smoothing(
-    #     Ieks(motion_model, meas_model, num_iter), states, measurements, prior_mean, prior_cov, cost_fn_eks, init_traj
-    # )
-    # results.append(
-    #     (ms_ieks, Ps_ieks, cost_ieks[1:], "IEKS"),
-    # )
-    # log.info("Running LM-IEKS...")
-    # ms_lm_ieks, Ps_lm_ieks, cost_lm_ieks, rmses_lm_ieks, neeses_lm_ieks = run_smoothing(
-    #     LmIeks(motion_model, meas_model, num_iter, 10, lambda_, 10),
-    #     states,
-    #     measurements,
-    #     prior_mean,
-    #     prior_cov,
-    #     cost_fn_eks,
-    #     init_traj,
-    # )
-    # results.append(
-    #     (ms_lm_ieks, Ps_lm_ieks, cost_lm_ieks[1:], "LM-IEKS"),
-    # )
-
-    log.info("Running LS-IEKS...")
-    dir_der_eks = partial(
-        dir_der_analytical_smoothing_cost,
-        measurements=measurements,
-        m_1_0=prior_mean,
-        P_1_0=prior_cov,
-        motion_model=motion_model,
-        meas_model=meas_model,
+    log.info("Running IEKS...")
+    ms_ieks, Ps_ieks, cost_ieks, rmses_ieks, neeses_ieks = run_smoothing(
+        Ieks(motion_model, meas_model, num_iter), states, measurements, prior_mean, prior_cov, cost_fn_eks, init_traj
     )
-    ms_ls_ieks, Ps_ls_ieks, cost_ls_ieks, rmses_ls_ieks, neeses_ls_ieks = run_smoothing(
-        LsIeks(
-            motion_model,
-            meas_model,
-            num_iter,
-            ArmijoLineSearch(cost_fn_eks, dir_der_eks, c_1=0.1),
-        ),
+    results.append(
+        (ms_ieks, Ps_ieks, cost_ieks[1:], "IEKS"),
+    )
+    log.info("Running LM-IEKS...")
+    ms_lm_ieks, Ps_lm_ieks, cost_lm_ieks, rmses_lm_ieks, neeses_lm_ieks = run_smoothing(
+        LmIeks(motion_model, meas_model, num_iter, 10, lambda_, 10),
         states,
         measurements,
         prior_mean,
@@ -161,70 +134,97 @@ def main():
         init_traj,
     )
     results.append(
-        (ms_ls_ieks, Ps_ls_ieks, cost_ls_ieks[1:], "LS-IEKS"),
+        (ms_lm_ieks, Ps_lm_ieks, cost_lm_ieks[1:], "LM-IEKS"),
     )
+
+    # log.info("Running LS-IEKS...")
+    # dir_der_eks = partial(
+    #     dir_der_analytical_smoothing_cost,
+    #     measurements=measurements,
+    #     m_1_0=prior_mean,
+    #     P_1_0=prior_cov,
+    #     motion_model=motion_model,
+    #     meas_model=meas_model,
+    # )
+    # ms_ls_ieks, Ps_ls_ieks, cost_ls_ieks, rmses_ls_ieks, neeses_ls_ieks = run_smoothing(
+    #     LsIeks(
+    #         motion_model,
+    #         meas_model,
+    #         num_iter,
+    #         ArmijoLineSearch(cost_fn_eks, dir_der_eks, c_1=0.1),
+    #     ),
+    #     states,
+    #     measurements,
+    #     prior_mean,
+    #     prior_cov,
+    #     cost_fn_eks,
+    #     init_traj,
+    # )
+    # results.append(
+    #     (ms_ls_ieks, Ps_ls_ieks, cost_ls_ieks[1:], "LS-IEKS"),
+    # )
 
     sigma_point_method = SphericalCubature()
 
-    # cost_fn_ipls = partial(
-    #     slr_smoothing_cost_pre_comp, measurements=measurements, m_1_0=prior_mean, P_1_0_inv=np.linalg.inv(prior_cov)
-    # )
+    cost_fn_ipls = partial(
+        slr_smoothing_cost_pre_comp, measurements=measurements, m_1_0=prior_mean, P_1_0_inv=np.linalg.inv(prior_cov)
+    )
 
-    # log.info("Running IPLS...")
-    # ms_ipls, Ps_ipls, cost_ipls, rmses_ipls, neeses_ipls = run_smoothing(
-    #     SigmaPointIpls(motion_model, meas_model, sigma_point_method, num_iter),
-    #     states,
-    #     measurements,
-    #     prior_mean,
-    #     prior_cov,
-    #     None,
-    #     init_traj,
-    # )
-    # results.append(
-    #     (ms_ipls, Ps_ipls, cost_ipls, "IPLS"),
-    # )
+    log.info("Running IPLS...")
+    ms_ipls, Ps_ipls, cost_ipls, rmses_ipls, neeses_ipls = run_smoothing(
+        SigmaPointIpls(motion_model, meas_model, sigma_point_method, num_iter),
+        states,
+        measurements,
+        prior_mean,
+        prior_cov,
+        None,
+        init_traj,
+    )
+    results.append(
+        (ms_ipls, Ps_ipls, cost_ipls, "IPLS"),
+    )
 
-    # log.info("Running LM-IPLS...")
-    # ms_lm_ipls, Ps_lm_ipls, cost_lm_ipls, rmses_lm_ipls, neeses_lm_ipls = run_smoothing(
-    #     SigmaPointLmIpls(
-    #         motion_model, meas_model, sigma_point_method, num_iter, cost_improv_iter_lim=10, lambda_=lambda_, nu=10
-    #     ),
-    #     states,
-    #     measurements,
-    #     prior_mean,
-    #     prior_cov,
-    #     cost_fn_ipls,
-    #     init_traj,
-    # )
-    # results.append(
-    #     (ms_lm_ipls, Ps_lm_ipls, cost_lm_ipls, "LM-IPLS"),
-    # )
+    log.info("Running LM-IPLS...")
+    ms_lm_ipls, Ps_lm_ipls, cost_lm_ipls, rmses_lm_ipls, neeses_lm_ipls = run_smoothing(
+        SigmaPointLmIpls(
+            motion_model, meas_model, sigma_point_method, num_iter, cost_improv_iter_lim=10, lambda_=lambda_, nu=10
+        ),
+        states,
+        measurements,
+        prior_mean,
+        prior_cov,
+        cost_fn_ipls,
+        init_traj,
+    )
+    results.append(
+        (ms_lm_ipls, Ps_lm_ipls, cost_lm_ipls, "LM-IPLS"),
+    )
 
-    # cost_fn_ls_ipls = partial(
-    #     slr_smoothing_cost_means,
-    #     measurements=measurements,
-    #     m_1_0=prior_mean,
-    #     P_1_0_inv=np.linalg.inv(prior_cov),
-    #     motion_fn=motion_model.map_set,
-    #     meas_fn=meas_model.map_set,
-    #     slr_method=SigmaPointSlr(sigma_point_method),
-    # )
+    cost_fn_ls_ipls = partial(
+        slr_smoothing_cost_means,
+        measurements=measurements,
+        m_1_0=prior_mean,
+        P_1_0_inv=np.linalg.inv(prior_cov),
+        motion_fn=motion_model.map_set,
+        meas_fn=meas_model.map_set,
+        slr_method=SigmaPointSlr(sigma_point_method),
+    )
 
-    # log.info("Running LS-IPLS...")
-    # ms_ls_ipls, Ps_ls_ipls, cost_ls_ipls, rmses_ls_ipls, neeses_ls_ipls = run_smoothing(
-    #     SigmaPointLsIpls(
-    #         motion_model, meas_model, sigma_point_method, num_iter, partial(ArmijoLineSearch, c_1=0.1), 10
-    #     ),
-    #     states,
-    #     measurements,
-    #     prior_mean,
-    #     prior_cov,
-    #     cost_fn_ls_ipls,
-    #     init_traj,
-    # )
-    # results.append(
-    #     (ms_ls_ipls, Ps_ls_ipls, cost_ls_ipls, "LS-IPLS"),
-    # )
+    log.info("Running LS-IPLS...")
+    ms_ls_ipls, Ps_ls_ipls, cost_ls_ipls, rmses_ls_ipls, neeses_ls_ipls = run_smoothing(
+        SigmaPointLsIpls(
+            motion_model, meas_model, sigma_point_method, num_iter, partial(ArmijoLineSearch, c_1=0.1), 10
+        ),
+        states,
+        measurements,
+        prior_mean,
+        prior_cov,
+        cost_fn_ls_ipls,
+        init_traj,
+    )
+    results.append(
+        (ms_ls_ipls, Ps_ls_ipls, cost_ls_ipls, "LS-IPLS"),
+    )
 
     for ms, _, _, label in results:
         tikz_2d_traj(Path.cwd() / "tikz", ms[:, :2], label)
